0a59f418631fec5fe09ebb9604fe3954ac47d4e4
Eduardo Flores <eduflornet@gmail.com>
2016-07-21 00:03:42 +0200

    Angular2 + TypeScript + NodeJS + Visual Studio Code
    Ejemplos Angular2 con TypeScript

---
+++ angular2/app/authors.component.ts
@@ -0,0 +1,20 @@
+import {Component} from 'angular2/core'
+import {AuthorService} from './author.service'
+
+@Component({
+    selector:'authors',
+    template:`<h2>Authors</h2>
+    <ul>
+    <li *ngFor="#author of authors">
+    {{author}}
+    </ul>
+    `,
+    providers:[AuthorService]
+})
+
+export class AuthorsComponent{
+    authors;
+    constructor(authorService:AuthorService){
+        this.authors=authorService.getAuthors();
+    }
+}
